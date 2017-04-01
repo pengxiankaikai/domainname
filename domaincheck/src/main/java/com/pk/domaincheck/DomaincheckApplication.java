2f45cb8c60aa1ee6b10747c91f117d3920393c59
karl <2973821510@qq.com>
2017-04-01 15:06:01 +0800

    check - init

---
+++ domaincheck/src/main/java/com/pk/domaincheck/DomaincheckApplication.java
@@ -0,0 +1,12 @@
+package com.pk.domaincheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomaincheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomaincheckApplication.class, args);
+	}
+}
